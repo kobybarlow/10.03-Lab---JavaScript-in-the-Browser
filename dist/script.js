13c65ad02454bde32c66cf7ee610d0e9e578339c
kobybarlow <kobybarlow@u.boisestate.edu>
2025-10-31 18:11:24 +0000

    Lab Activity Completed

--- dist/script.js
+++ dist/script.js
@@ -1,20 +1,31 @@
-// This is a simple JavaScript file that adds interactivity to the HTML page
-// It defines a function to show an alert when a link is clicked
-function sayHello() {
-    alert("Hello, world from javascript!");
-}
-// This function will be called when the link is clicked
-// It shows an alert with a message
-// Ensure the DOM is fully loaded before attaching the event listener
-document.addEventListener("DOMContentLoaded", function() {
-    const link = document.getElementById("hello-link");
-    if (!link) {
-        console.error("Link with ID 'hello-link' not found.");
-        return;
-    }
-    link.addEventListener("click", function(event) {
-        event.preventDefault(); // Prevent the default link behavior
-        sayHello();
-    });
-});
 
+
+// Form Submission listener
+document.getElementById("lineupForm").addEventListener("submit", myLineup);
+
+function myLineup(event) {
+    event.preventDefault();
+
+    // Get Status
+    const status = document.querySelector('input[name="status"]:checked').value;
+
+    // Get Date
+    const date = document.getElementById("Dates").value;
+
+
+    // Convert Node list into array
+    const checkedPlayers = document.querySelectorAll('input[name="players"]:checked');
+    const Players = Array.from(checkedPlayers).map(p => p.value);
+
+
+    // Print all selected information
+    console.log("status: ", status);
+    console.log("Date: ", date);
+    console.log("Players: ", Players.join(" , "));
+
+    const lineupInfo = `Status: ${status} | Date: ${date} | Players: ${Players.join(" , ")}`;
+
+
+    // Display the string onto the page
+    document.getElementById("Output").textContent = lineupInfo;
+}
